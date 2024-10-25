27cfbb9e53f1cc8e965730f3f23adbf341708c67
Pedro A. <pedroaaccioly@gmail.com>
2024-10-24 22:18:25 -0300

    Update crawl_uidata.py
    Small fixes

--- crawl_uidata.py
+++ crawl_uidata.py
@@ -6,20 +6,22 @@ import csv
 import argparse
 from tqdm import tqdm
 import sys
-from .utils import *
+from utils import *
 
 parser = argparse.ArgumentParser()
     
-parser.add_argument("--o", help='Absolute path of output data folder.', required=True)
-parser.add_argument("--i", help='Absolute path of input url text file.', required=True)
+parser.add_argument("-o", help='Absolute path of output data folder.', required=True)
+parser.add_argument("-i", help='Absolute path of input url text file.', required=True)
 
-#args = parser.parse_args()
+args = parser.parse_args()
 
+# Access the arguments from the args object
 folder_path = args.o
 url_file = args.i
 
-# Extract all the urls to crawl from the url file
+# Extract all the URLs to crawl from the input URL file
 website_urls = collect_web_urls(url_file)
 
-# Crawl all web-urls and save files with image urls, corresponding preceding and succeeding text.
+# Crawl all web URLs and save files with image URLs, 
+# corresponding preceding and succeeding text.
 extract_images(website_urls, folder_path)
